d431ad59a6b3ce99476d4b98f3f025d128c72992
bovbo <96370622+bovbo@users.noreply.github.com>
2023-05-03 16:12:16 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,7 +18,7 @@ sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generat
 
 
 # 更新
- ./scripts/feeds update -a && ./scripts/feeds install -a
+ #./scripts/feeds update -a && ./scripts/feeds install -a
 
 ##########
 # Modify the version number
