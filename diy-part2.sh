4b0cce9e89889f96d3efe0c1df42c1ff89e912ec
bovbo <96370622+bovbo@users.noreply.github.com>
2023-05-23 16:05:56 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,17 +12,14 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
-# Modify hostname
-#sed -i 's/OpenWrt/Acrh17/g' package/base-files/files/bin/config_generate
 
 
+rm -rf  feeds/luci/themes/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 
 # 更新
- #./scripts/feeds update -a && ./scripts/feeds install -a
+ ./scripts/feeds update -a && ./scripts/feeds install -a
 
-##########
-# Modify the version number
-#sed -i "s/OpenWrt /Leopard build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
 
 
 
