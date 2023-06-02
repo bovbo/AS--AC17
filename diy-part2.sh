032c67695ffab3299e9304df8498fec0a2daa62c
bovbo <96370622+bovbo@users.noreply.github.com>
2023-06-02 21:47:31 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,7 +9,7 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
-
+chmod -R 755 files
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
 # 取消bootstrap为默认主题
